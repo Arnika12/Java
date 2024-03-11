d71a2c4f6aca47ebc864b5d18e14839f78af836c
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2024-03-11 09:27:10 +0530

    Update FileClient.java

--- JAVA/Networking/Display contents of file on client machine/FileClient.java
+++ JAVA/Networking/Display contents of file on client machine/FileClient.java
@@ -1,3 +1,9 @@
+/*3. Write a program which sends the name of a text file from the client to server and
+displays the contents of the file on the client machine. If the file is not found, display an
+error message. */
+
+// Client Side Code
+
 import java.io.*;
 import java.net.Socket;
 
