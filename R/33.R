0fb98b7b6007ba4858becf18cfafd99602998d91
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2024-03-14 14:08:54 +0530

    Add files via upload

---
+++ R/33.R
@@ -0,0 +1,5 @@
+# 33) Write a script in R to create a list of students and perform the following
+#     i) Give names to the students in the list. 
+#     ii) Add a student at the end of the list.
+#     iii) Remove the first Student.
+#     iv) Update the second last student.
