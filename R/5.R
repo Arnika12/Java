bed64cb281088bff3f037386bddbc3b65a7a52dd
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2024-03-14 13:45:52 +0530

    Add files via upload

---
+++ R/5.R
@@ -0,0 +1,17 @@
+# 5. Write a R program to multiply two vectors of integers type and length 3.
+
+v1 <- c(1,2,3)
+v2 <- c(4,5,6)
+print(v1)
+print(v2)
+
+c1 <- v1 + v2
+c2 <- v1 - v2
+c3 <- v1 * v2
+c4 <- v1 / v2
+
+print(c1)
+print(c2)
+print(c3)
+print(c4)
+
