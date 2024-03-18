7dd6fb27c8e244768d1f82cef5edd5c2e86fc3eb
Arnika12 <arnikagunjal19@gmail.com>
2024-03-18 22:54:53 +0530

    added files

--- R/33.R
+++ R/33.R
@@ -3,3 +3,21 @@
 #     ii) Add a student at the end of the list.
 #     iii) Remove the first Student.
 #     iv) Update the second last student.
+
+students_list <- list("Aditi", "Aarav", "Rucha", "Neha")
+
+# i)
+names(students_list) <- c("Student1", "Student2", "Student3", "Student4")
+print(students_list)
+
+# ii)
+students_list <- c(students_list, "Kavya")
+print(students_list)
+
+# iii)
+students_list <- students_list[-1]     # -1 number of student to remove from list
+print(students_list)
+
+# iv)
+students_list[length(students_list) - 1] <- "Rohan"
+print(students_list)
