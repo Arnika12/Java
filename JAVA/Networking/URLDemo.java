9b7fd0b1caad62475ef88ef5f353aff7b2145d16
Arnika12 <arnikagunjal19@gmail.com>
2024-03-22 10:14:27 +0530

    modified url demo

--- JAVA/Networking/URLDemo.java
+++ JAVA/Networking/URLDemo.java
@@ -1,5 +1,7 @@
 							//URL Class Demo
-//write a Java program that demonstrates the use of the URL class to extract information from a website URL. The program prompts the user to input a website address (e.g., https://www.facebook.com/profile) and then displays the protocol, host name, port number, and file name of the URL. Review the code and answer the questions below:
+/*write a Java program that demonstrates the use of the URL class to extract information from a 
+website URL. The program prompts the user to input a website address (e.g., https://www.facebook.com/profile) 
+and then displays the protocol, host name, port number, and file name of the URL. */
 
 import java.io.*;
 import java.net.*;
