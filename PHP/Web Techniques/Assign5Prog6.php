8dd551b5c176fa2fbe510d782f688c8c2bd6dcfb
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2024-03-23 10:00:59 +0530

    Add files via upload

---
+++ PHP/Web Techniques/Assign5Prog6.php
@@ -0,0 +1,16 @@
+<!-- 6. Write a PHP script to change the preference of your web page like font style, font,
+size, font color, background color using cookie.
+Display selected settings on next page and actual implementation (with new settings)
+on third page.-->
+
+<?php
+echo "Style is ".$_GET['s1']."
+Color is ".$_GET['c']."
+Background color is ".$_GET['b']."
+size is ".$_GET['s'];
+setcookie("set1",$_GET['s1'],time()+3600);
+setcookie("set2",$_GET['c'],time()+3600);
+setcookie("set3",$_GET['b'],time()+3600);
+setcookie("set4",$_GET['s'],time()+3600);
+?><br>
+<a href="Assign5Prog6_1.php">Show</a>
