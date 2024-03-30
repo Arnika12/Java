073a08e3a468b4e3ece7f117252016f468a40b3f
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2024-03-30 22:13:09 +0530

    Create P5.php

---
+++ PHP/Web Techniques/P5.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>PHP program to Count Number of Visits on a web page using cookies in php</title>
+</head>
+<body>
+
+    <h3>Write a PHP Program to Count Number of Visits on a web page using cookies in php</h3>
+
+    <?php
+        $countvisit = 1;
+
+        if(isset($_COOKIE['countvisit']))
+        {
+            $countvisit = $_COOKIE['countvisit'];
+            $countvisit ++; 
+        }
+        setcookie('countvisit', $countvisit);       
+        echo "You have visited ".$countvisit." times";
+    ?>
+    
+</body>
+</html>
