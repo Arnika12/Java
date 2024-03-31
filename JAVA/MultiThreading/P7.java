14eea153509490046fa045a7c66028fadd31b7f5
Arnika12 <arnikagunjal19@gmail.com>
2024-03-31 19:13:49 +0530

    added

---
+++ JAVA/MultiThreading/P7.java
@@ -0,0 +1,2 @@
+/*7. Write a program in which thread sleep for 6 sec in the loop in reverse order from 100 to 1 
+    and change the name of thread. */ 
---
+++ JAVA/MultiThreading/P8.java
@@ -0,0 +1,3 @@
+/*8. Write a program to solve producer consumer problem in which a producer produces a value and 
+    consumer consume the value before producer generate the next value. 
+    (Hint: use thread synchronization)*/
