e691984444541f537c8b6b72cc41abf1b39c1e17
Arnika Gunjal <146057157+Arnika12@users.noreply.github.com>
2024-04-24 11:20:02 +0530

    Inserted code for basic operation on table

---
+++ JAVA/Database Connectivity/Update.java
@@ -0,0 +1,44 @@
+//Update values in table
+
+import java.sql.*;
+import java.io.*;
+
+public class Update{
+public static void main (String args[]){
+
+try{
+	
+	Class.forName("org.postgresql.Driver");
+	Connection con=null;
+	Statement stmt= null;
+	ResultSet rs=null;
+	con=DriverManager.getConnection("jdbc:postgresql:test","postgres","");
+	
+	if(con==null){
+		System.out.println("connection error");
+	}
+	else{
+	stmt=con.createStatement();
+	stmt.executeUpdate("Update Person SET name='priay' where pid=6");
+	
+	rs=stmt.executeQuery("select * from Person;");
+	while (rs.next()){
+		System.out.println("PID : "+ rs.getInt(1));
+		System.out.println("Name : "+ rs.getString(2));
+		System.out.println("Gender : "+ rs.getString(3));
+		System.out.println("Birth_year : "+ rs.getInt(4));
+	}
+	
+}
+}
+catch(ClassNotFoundException e){
+	e.printStackTrace();
+}
+catch(SQLException ex1){
+	System.out.println("error"+ex1);
+}
+catch(Exception ex2){
+	System.out.println("error"+ex2);
+}
+}
+}
